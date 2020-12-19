1d14570fa3002bc54b1a83d4050402e08f3930c1
michalskirobert <majointa@gmail.com>
2020-12-19 22:26:30 +0100

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum,
+        libero? Et sit dolores quod nemo doloremque quos fugit, optio harum
+        molestias ipsum rerum saepe officia temporibus praesentium voluptatem
+        velit culpa. Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+        Quisquam quo quis voluptatum veniam dignissimos minima itaque
+        praesentium, amet necessitatibus distinctio dicta molestiae porro,
+        aspernatur ex officiis a magni eveniet reiciendis?
+      </p>
+    </section>
+  );
+};
+
+export default About;
